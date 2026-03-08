4fd56c49ac705dc0061734ca65d80cf99e6097cc
Ashwin Goyal <ashwin.goyal1@ibm.com>
2026-03-08 03:33:35 -0400

    add DB layer

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
+from app.db.base import Base
+from app.db.session import engine
+
 app = FastAPI(title="Content Creator Camera Webapp API")
+
+Base.metadata.create_all(bind=engine)
 
 
 @app.get("/health")
